9ca245bfd6025f853059746c1176358ea420974a
Waqas Hakeem <waqas@rnssol.com>
2025-06-27 15:17:03 +0500

    update: Setting page responsive set

--- src/components/user-profile/UserMetaCard.tsx
+++ src/components/user-profile/UserMetaCard.tsx
@@ -28,9 +28,9 @@ export default function UserMetaCard() {
   };
   return (
     <>
-      <div className="p-5 border border-gray-200 rounded-2xl dark:border-gray-800 lg:p-6">
-        <div className="flex flex-col gap-5 xl:items-center xl:justify-between">
-          <div className="flex flex-col items-center w-full gap-6 xl:flex-row">
+      <div className="p-3 border border-gray-200 rounded-2xl dark:border-gray-800 sm:p-6">
+        <div className="flex flex-col gap-5 md:items-center md:justify-between">
+          <div className="flex flex-col items-center w-full gap-6 md:flex-row">
             <div className="w-20 h-20 overflow-hidden border border-gray-200 rounded-full dark:border-gray-800">
               <Image
                 width={80}
@@ -39,11 +39,11 @@ export default function UserMetaCard() {
                 alt="user"
               />
             </div>
-            <div className="order-3 xl:order-2">
-              <h4 className="mb-2 text-lg font-semibold text-center text-gray-800 dark:text-white/90 xl:text-left">
+            <div className="order-3 md:order-2">
+              <h4 className="mb-2 text-lg font-semibold text-center text-gray-800 dark:text-white/90 md:text-left">
                 {user?.name}
               </h4>
-              <div className="flex flex-col items-center gap-1 text-center xl:flex-row xl:gap-3 xl:text-left">
+              <div className="flex flex-col items-center gap-1 text-center flex-row :gap-3 md:text-left">
                 <p className="text-sm text-gray-500 dark:text-gray-400">
                   {user?.email}
                 </p>
@@ -78,8 +78,8 @@ export default function UserMetaCard() {
           {editEmail && (
             <div className="flex items-center w-full gap-6 flex-row">
               {/* <form className="flex flex-col"> */}
-              <div className="flex items-end flex-row gap-6 px-2 pb-3">
-                <div className="flex flex-col">
+              <div className="flex items-center sm:items-end flex-col sm:flex-row gap-6 px-2 pb-3 w-full sm:w-auto">
+                <div className="flex flex-col w-full">
                   <Label>Email Address</Label>
                   <Input type="text" defaultValue={user?.email} />
                 </div>
