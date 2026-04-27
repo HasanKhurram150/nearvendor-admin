ebbe52c1113169037e4a37af3276d0501686eec0
HasanKhurram150 <hasankhurram150@gmail.com>
2026-04-27 16:03:05 +0500

    feat: uncommented page route

---
+++ src/app/(admin)/(others-pages)/vendor-application/page.tsx
@@ -0,0 +1,12 @@
+import VendorApplication from "@/components/vendor-application/VendorApplication";
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "Vendor Application",
+  description: "Vendor Application",
+};
+
+export default function page() {
+  return <VendorApplication />;
+}
